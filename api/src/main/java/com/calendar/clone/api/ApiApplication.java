12904c5d8dc9412bae5f71db6f90f6722ab79773
Kim kyunghee <kkh2415@naver.com>
2022-07-29 21:18:17 +0900

    fix : @SpringBootApplication애노테이션에 scanBasePackages 옵션 설정

--- api/src/main/java/com/calendar/clone/api/ApiApplication.java
+++ api/src/main/java/com/calendar/clone/api/ApiApplication.java
@@ -3,11 +3,12 @@ package com.calendar.clone.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EntityScan("com.calendar.clone.core")
 @EnableJpaRepositories("com.calendar.clone.core")
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.calendar.clone")
 public class ApiApplication {
 
     public static void main(String[] args) {
