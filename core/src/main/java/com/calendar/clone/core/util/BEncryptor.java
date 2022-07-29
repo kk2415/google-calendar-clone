1368a26f40cabcf0f7cbaf7a49dc2de75b2eb8c4
Kim kyunghee <kkh2415@naver.com>
2022-07-29 21:17:37 +0900

    feat : BEncrypt hash generator 생성

---
+++ core/src/main/java/com/calendar/clone/core/util/BEncryptor.java
@@ -0,0 +1,22 @@
+package com.calendar.clone.core.util;
+
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BEncryptor implements Encryptor {
+
+    @Override
+    public String encrypt(String origin) {
+        return BCrypt.hashpw(origin, BCrypt.gensalt());
+    }
+
+    @Override
+    public boolean isMatch(String origin, String hashed) {
+        try {
+            return BCrypt.checkpw(origin, hashed);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
---
+++ core/src/main/java/com/calendar/clone/core/util/Encryptor.java
@@ -0,0 +1,7 @@
+package com.calendar.clone.core.util;
+
+public interface Encryptor {
+
+    String encrypt(String origin);
+    boolean isMatch(String origin, String hashed);
+}
